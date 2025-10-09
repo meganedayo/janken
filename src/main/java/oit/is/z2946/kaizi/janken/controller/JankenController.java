1e5d529cf6445f96d2ff29fdae9f13970f7e0948
meganedayo <e1b23021@oit.ac.jp>
2025-10-09 12:15:35 +0900

    じゃんけんの手を選んでCPUと対戦する処理実装完了

--- src/main/java/oit/is/z2946/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2946/kaizi/janken/controller/JankenController.java
@@ -1,6 +1,7 @@
 package oit.is.z2946.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // Modelをインポート
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,29 +10,47 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class JankenController {
 
-  /**
-   * janken.html を表示します。
-   *
-   * @return janken.htmlのテンプレート名
-   */
   @GetMapping("/janken")
   public String jankenPage() {
     return "janken.html";
   }
 
-  /**
-   * index.htmlからのログイン情報を受け取り、jankenページにリダイレクトします。
-   *
-   * @param userName           フォームから送られてきたユーザ名
-   * @param redirectAttributes リダイレクト先に情報を引き継ぐためのオブジェクト
-   * @return リダイレクト先のパス
-   */
   @PostMapping("/janken/login")
   public String login(@RequestParam("username") String userName, RedirectAttributes redirectAttributes) {
-    // リダイレクト先に一時的な属性（Flash Attribute）としてユーザ名を追加
     redirectAttributes.addFlashAttribute("username", userName);
-
-    // /janken へリダイレクト
     return "redirect:/janken";
+  }
+
+  /**
+   * じゃんけんの対戦処理を行う
+   *
+   * @param myHand プレイヤーが選んだ手
+   * @param model  テンプレートに渡すデータを格納するオブジェクト
+   * @return janken.htmlのテンプレート名
+   */
+  @GetMapping("/janken/play")
+  public String play(@RequestParam("myhand") String myHand, Model model) {
+    // CPUの手を「グー」に固定
+    String cpuHand = "グー";
+
+    // 勝敗判定
+    String result;
+    if (myHand.equals(cpuHand)) {
+      result = "あいこ";
+    } else if ((myHand.equals("グー") && cpuHand.equals("チョキ")) ||
+        (myHand.equals("チョキ") && cpuHand.equals("パー")) ||
+        (myHand.equals("パー") && cpuHand.equals("グー"))) {
+      result = "あなたの勝ち！";
+    } else {
+      result = "あなたの負け...";
+    }
+
+    // テンプレートに渡すデータをModelオブジェクトに追加
+    model.addAttribute("myHand", myHand);
+    model.addAttribute("cpuHand", cpuHand);
+    model.addAttribute("result", result);
+
+    // janken.html を表示
+    return "janken";
   }
 }
